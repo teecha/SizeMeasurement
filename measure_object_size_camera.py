630dbd111580768342d69a5ba86db3325ddbe160
Teja Madduri <teecha23@gmail.com>
2022-12-05 21:54:22 -0700

    final commit

--- measure_object_size_camera.py
+++ measure_object_size_camera.py
@@ -11,7 +11,7 @@ aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_100)
 detector = HomogeneousBgDetector()
 
 # Load Cap
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
